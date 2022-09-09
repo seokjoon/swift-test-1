1acaefa1c34cfbd35228b181c3a397059dfe1c7b
seokjoon <seokjoon@gmail.com>
2022-09-09 22:30:49 +0900

    outlet, action

--- swift-test-1/ViewController.swift
+++ swift-test-1/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var ButtonFoo: UIButton!
 
+    @IBOutlet weak var LabelFoo: UILabel!
+
+    @IBOutlet var TextFoo: UITextField!
+    
+    @IBAction func ActionButtonFoo(_ sender: UIButton) {
+        LabelFoo.text = "Foo: " + TextFoo.text!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
